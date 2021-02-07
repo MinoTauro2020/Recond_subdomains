830634622b991e3abacb6465768d6e11342eed59
MinoTauro2020 <ahfseguridad@gmail.com>
2021-02-07 11:47:19 -0500

    funcion httpx

--- Recond_subdomains.sh
+++ Recond_subdomains.sh
@@ -11,33 +11,29 @@ function ctrl_c(){
 	exit 0
 }
 
-function checktools(){
-
-  dependencies=(html2text jq)
-
-	for program in $dependencies ; do
-	test -f /usr/bin/$program
-	if ["$(echo $?)" == "0"] ; then
-	echo "no esta instalada";
-	sleep 2
-	echo "installing"
-	apt-get install html2text
-	echo "finish"
-	else
-	echo "All tools are installed OK!"
-	fi;
-	done
-
-}
 
 function CheckHost(){
 
 	clear
+
 	echo "Checking hosts alive"
+
 	cat $host-sub-total | httpx -silent | tee $host-alive
 
-}
+	wc -l $host-alive
 
+	echo -n "Do you want to use nuclei to find vulnerabilities: "
+
+	read answer
+
+	if [ $answer =="yes"]
+        then
+	echo "ha elegido yes"
+	else
+	echo "ha elegido no"
+	fi
+
+}
 
 select opcion in $options;
 
@@ -47,21 +43,57 @@ do
 		clear
 		echo -n "Write the domain name : "
 		read host
+
 		echo "0%"
+
 		curl -s -d  domain=$host  -X POST https://osint.sh/subdomain/ | grep -Eo '(http|https)://[^/"]+' | grep $host >> $host-sub
+
+		echo "10%"
+
 		curl -s "https://crt.sh/?q=%25.$host&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' >> $host-sub
+		
+		echo "20%"
+
 		curl -s "https://dns.bufferover.run/dns?q=.$host" |jq -r .FDNS_A[] | sed -s 's/,/\n/g' >> $host-sub
+
+		echo "30%"
+
 		curl -s "https://www.threatcrowd.org/searchApi/v2/domain/report/?domain=$host"|jq -r '.subdomains' 2>/dev/null |grep -o "\w.*$host" | tr -d "\"," >> $host-sub
+
+		echo "40%"
+
 		curl -s "https://api.hackertarget.com/hostsearch/?q=$host" |grep -o "\w.*$host" >> $host-sub
+
+		echo "50%"
+
 		curl -s "https://rapiddns.io/subdomain/$host?full=1"  | grep -oP '_blank">\K[^<]*' >> $host-sub
+
+		echo "60%"
+
 		curl -s "https://crt.sh/?q=%25.$host&output=json" | jq -r '.[].name_value' | sed 's/\*.//' >> $host-sub
+
+		echo "70%"
+
 		curl -s "https://crt.sh/?CN=%25.$host&output=json" | jq -r '.[].name_value' | sed 's/\*.//' >> $host-sub
+
+		echo "80%"
+
 		curl -s "https://riddler.io/search/exportcsv?q=pld:$host" | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u >> $host-sub
+
+		echo "90%"
+
 		curl -s "https://securitytrails.com/list/apex_domain/$host" | grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | grep ".$host" | sort -u >> $host-sub
+
+		echo "100"
+
 		cat $host-sub | sort -u | uniq | tee $host-sub-total
-		sleep 1
-		wc -l $host-sub-total ; echo "Founded"
+
+		wc -l $host-sub-total && echo "Founded"
+
+		sleep 5
+
 		echo "===Checking hosts alive===="
+
 		CheckHost
 
 	elif [ $opcion = "Ports" ]; then
